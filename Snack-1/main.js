ff6b9b506347ce8062c7318e56454a50a64492b6
Marco Sandri <sandrimarco98@gmail.com>
2022-02-03 14:51:39 +0100

    Snack 1: altre biciclette con lo stesso peso

--- Snack-1/main.js
+++ Snack-1/main.js
@@ -13,7 +13,7 @@ const biciclette = [
     },
     {
         nome : "Merida",
-        peso : 6,
+        peso : 3,
     },
     {
         nome : "Trek",
@@ -33,7 +33,7 @@ const biciclette = [
     },
     {
         nome : "Santa Cruz",
-        peso : 5,
+        peso : 3,
     },
     {
         nome : "Giant",
@@ -41,9 +41,11 @@ const biciclette = [
     }
 ];
 
+// Prendo i valori del primo oggetto dell'array
 let {peso} = biciclette[0];
 let {nome} = biciclette[0];
 
+// Ciclo tutti i valori per vedere quello con il peso minore
 for(let i = 0; i < biciclette.length; i++) {
 
     if(peso > biciclette[i].peso) {
@@ -52,7 +54,22 @@ for(let i = 0; i < biciclette.length; i++) {
     }
 }
 
-console.log(nome);
-console.log(peso);
+// Stampo qual è la bicicletta con il peso minore
 console.log(`La bicicletta da corsa con il peso minore è ${nome} con ${peso} kg`);
+document.getElementById('print').innerText =  `La bicicletta da corsa con il peso minore è ${nome} con ${peso} kg`;
 
+// Nel caso ci fossero altre biciclette con lo stesso peso
+// Creo un array per contenerle
+let altreBiciclette = [];
+
+// Ciclo tutti i valori per vedere le bici con lo stesso peso di quella con il peso minore trovata in precedenza e aggiungo i loro nomi all'array
+for(let i = 0; i < biciclette.length; i++) {
+
+    if(peso == biciclette[i].peso && nome !== biciclette[i].nome) {
+         altreBiciclette.push(biciclette[i].nome);
+    }
+}
+
+// Stampo tutte le altre biciclette con lo stesso peso
+console.log(`Altre biciclette che pesano ${peso} kg sono: ${altreBiciclette}` )
+document.getElementById('printOther').innerText =  `Altre biciclette che pesano ${peso} kg sono: ${altreBiciclette}`;
